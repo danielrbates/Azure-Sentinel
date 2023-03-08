d96f3fae38b36d248db61f453a829c67ec792051
Dvir Naim <106969883+dvir-ms@users.noreply.github.com>
2023-03-08 16:32:54 +0200

    Update sapcon-sentinel-ui-agent-kickstart.sh

--- Solutions/SAP/sapcon-sentinel-ui-agent-kickstart.sh
+++ Solutions/SAP/sapcon-sentinel-ui-agent-kickstart.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-for folder in */ ; do
-  if [ -f "$folder/systemconfig.ini" ]; then
-    sysnr=$(grep 'sysnr' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
-    ashost=$(grep 'ashost' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
-    sysid=$(grep 'sysid' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')    
-    mshost=$(grep 'mshost' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
-    echo "mshost = $mshost"
-    echo "sysnr = $sysnr"
-    echo "ashost = $ashost"
-    echo "sysid = $sysid"
-    echo "****"
-  fi
+
+# Loop over all running Docker containers
+for container_id in $(docker ps -q)
+do
+    # Get the container name
+    container_name=$(docker inspect --format '{{ .Name }}' $container_id | sed 's|/||')
+
+    # Check if the container has the SENTINEL_AGENT_GUID environment variable
+    sentinel_agent_guid=$(docker inspect --format '{{range $index, $value := .Config.Env}}{{if eq (index (split $value "=") 0) "SENTINEL_AGENT_GUID"}}{{index (split $value "=") 1}}{{end}}{{end}}' $container_id)
+
+    if [[ -n $sentinel_agent_guid ]]; then
+        # Print the container name and SENTINEL_AGENT_GUID value
+        echo "Container: $container_name"
+        echo "SENTINEL_AGENT_GUID: $sentinel_agent_guid"
+        echo ""
+    fi
 done
+
