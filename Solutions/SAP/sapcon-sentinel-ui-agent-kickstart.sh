5832e587c46d335650fe7bc7a4588fc1bdecc0a8
Dvir Naim <106969883+dvir-ms@users.noreply.github.com>
2023-03-08 15:22:59 +0200

    Update sapcon-sentinel-ui-agent-kickstart.sh

--- Solutions/SAP/sapcon-sentinel-ui-agent-kickstart.sh
+++ Solutions/SAP/sapcon-sentinel-ui-agent-kickstart.sh
@@ -3,7 +3,9 @@ for folder in */ ; do
   if [ -f "$folder/systemconfig.ini" ]; then
     sysnr=$(grep 'sysnr' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
     ashost=$(grep 'ashost' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
-    sysid=$(grep 'sysid' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
+    sysid=$(grep 'sysid' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')    
+    mshost=$(grep 'mshost' "$folder/systemconfig.ini" | cut -d '=' -f 2 | tr -d ' ')
+    echo "mshost = $mshost"
     echo "sysnr = $sysnr"
     echo "ashost = $ashost"
     echo "sysid = $sysid"
